38375637cc6027256b5f111be2ff8dca99738385
NMIRI-kmatsubara <matsubara.kauzne@nmiri.city.nagoya.jp>
2020-02-26 11:40:44 +0900

    add get temp server

--- start_http_server.py
+++ start_http_server.py
@@ -1,18 +1,46 @@
 from wsgiref.simple_server import make_server
  
 import json
- 
- 
+import smbus
+import datetime
+
+def get_temp():
+    bus = smbus.SMBus(1)
+
+    address = 0x48
+    register = 0x00
+    configration = 0x03
+
+    bus.write_word_data(address, configration, 0x00)
+    word_data = bus.read_word_data(address, register)
+
+    print(hex(word_data))
+
+    data = (word_data & 0xff00 ) >> 8 |(word_data & 0xff) << 8
+    
+    data = data >> 3
+    
+    return (data/16.)
+
+
 def app(environ, start_response):
-  status = '200 OK'
-  headers = [
-    ('Content-type', 'application/json; charset=utf-8'),
-    ('Access-Control-Allow-Origin', '*'),
-  ]
-  start_response(status, headers)
+    status = '200 OK'
+    headers = [
+      ('Content-type', 'application/json; charset=utf-8'),
+      ('Access-Control-Allow-Origin', '*'),
+    ]
+    start_response(status, headers)
+    
+    temp = get_temp()
+    now = datetime.datetime.now().strftime("%H:%M:%S")
+   
+    return [json.dumps({'date': now ,'temp': temp }).encode("utf-8")]
  
-  return [json.dumps({'message':'hoge'}).encode("utf-8")]
  
-with make_server('', 3000, app) as httpd:
-  print("Serving on port 3000...")
-  httpd.serve_forever()
+def main():
+  #with make_server('', 3000, app) as httpd:
+    print("Serving on port 3000...")
+    make_server('', 3000, app).serve_forever()
+    
+if __name__== "__main__":
+  main()
